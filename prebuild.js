e997cb57352e0c079dbf9860f9f1a0a310dc570f
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2020-10-27 14:30:06 +0500

    #2
    - исправлено формирование файлов на этапе prebuild

--- prebuild.js
+++ prebuild.js
@@ -1,18 +1,18 @@
 const fs = require('fs-extra');
 const path = require('path');
 const SVGO = require('svgo');
-const { default: svgr } = require('@svgr/core');
+const svgr = require('@svgr/core').default;
 const svgrConfig = require('./svgr.config');
 const { upperFirst, camelCase, kebabCase } = require('lodash');
 const { listAllFiles } = require('./build-utils');
 
 async function main () {
-  await preBuild(resolveArgs().tempDir);
+  await preBuild(parseArgs());
 
   console.log('pre build: success!');
 }
 
-const resolveArgs = () => {
+const parseArgs = () => {
   const arg = process.argv.slice(2).find(item => /^--tempDir=/.test(item));
 
   if (!arg) {
@@ -24,52 +24,27 @@ const resolveArgs = () => {
   };
 };
 
-async function preBuild (tempDirPath) {
-  const srcPath = path.join(__dirname, 'src/');
-  const tempPath = path.join(__dirname, `${tempDirPath}/`);
-  const buildPath = path.join(__dirname, 'build/');
-
-  await copySVG(srcPath, buildPath);
-  await createTSX(srcPath, tempPath);
-}
-
-async function copySVG (srcPath, targetPath) {
-  await fs.copy(srcPath, targetPath);
-
-  const files = (await listAllFiles(targetPath)).filter(file => path.extname(file.name) === '.svg');
-
+async function preBuild ({ tempDir: tsxDir }) {
+  const srcPath = path.join(process.cwd(), 'src/');
+  const files = await listAllFiles(srcPath, { ext: '.svg' });
   const svgo = new SVGO({ plugins: svgrConfig.svgoConfig.plugins });
 
   for (const file of files) {
+    const fileSource = await fs.readFile(file.path, 'utf8');
+
     const cleanName = path.basename(file.name, '.svg').replace('&', 'And');
     const fileNameBase = kebabCase(cleanName);
-    const optimizedSvgSource = await svgo.optimize(await fs.readFile(file.path), { path: file.path });
+    const componentName = `${upperFirst(camelCase(cleanName))}SVG`;
+    const basePath = file.dirname.replace(process.cwd() + '/src', '');
 
-    await fs.writeFile(path.join(file.dirname, `${fileNameBase}.svg`), optimizedSvgSource.data);
-  }
-}
+    const readySVG = (await svgo.optimize(fileSource, { path: file.path })).data;
+    const readyTSX = await svgr(fileSource, svgrConfig, { componentName });
 
-async function createTSX (srcPath, targetPath) {
-  // копируем все файлы из src во временный каталог
-  await fs.copy(srcPath, targetPath);
+    const svgPath = path.join(process.cwd(), 'build', basePath, `${fileNameBase}.svg`);
+    const tsxPath = path.join(process.cwd(), tsxDir, basePath, `${fileNameBase}.tsx`);
 
-  const tempFiles = (await listAllFiles(targetPath)).filter(file => path.extname(file.name) === '.svg');
-
-  // меняем каждый временный файл svg на временный файл tsx
-  for (const file of tempFiles) {
-    const cleanName = path.basename(file.name, '.svg').replace('&', 'And');
-
-    const fileNameBase = kebabCase(cleanName);
-    const componentName = upperFirst(camelCase(cleanName));
-
-    const svgCode = await fs.readFile(file.path, 'utf8');
-    const componentCode = await svgr(svgCode, svgrConfig, { componentName });
-
-    // меняем исходный код SVG на исходный код React-компонента
-    await fs.writeFile(file.path, componentCode);
-
-    // переименовываем файл в .tsx, исправляем имя на kebab-case
-    await fs.rename(file.path, path.join(file.dirname, `${fileNameBase}.tsx`));
+    await fs.outputFile(svgPath, readySVG);
+    await fs.outputFile(tsxPath, readyTSX);
   }
 }
 
