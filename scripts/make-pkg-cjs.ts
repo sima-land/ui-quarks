16a55b0f344d7c9a4f0e64a542f6bcaa213a50a1
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2023-10-25 16:19:19 +0500

    #62
    - правки для работы с MF

--- scripts/make-pkg-cjs.ts
+++ scripts/make-pkg-cjs.ts
@@ -2,6 +2,14 @@ import { readFile, outputFile } from 'fs-extra';
 
 readFile('./package.json', 'utf-8')
   .then(JSON.parse)
-  .then(({ name, version }) => ({ name, version, type: 'commonjs' }))
-  .then(data => outputFile('dist/cjs/package.json', JSON.stringify(data)))
+  .then(({ name, version, dependencies, peerDependencies }) => ({
+    type: 'commonjs',
+
+    // ВАЖНО: копируем эти поля для того чтобы устранить warning'и ModuleFederationPlugin
+    name,
+    version,
+    dependencies,
+    peerDependencies,
+  }))
+  .then(data => outputFile('dist/cjs/package.json', JSON.stringify(data, null, 2)))
   .then(() => console.log('[ui-quarks] CJS pkg emit done'));
